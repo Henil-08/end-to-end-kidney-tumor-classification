e8d2739cfe03a9a646d8a1c9772343f7ec2bd4c4
“Henil <henilgajjar08@gmail.com>
2024-06-15 17:18:23 -0400

    Main

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tumorClassifier import logger
-from tumorClassifier.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline, STAGE_NAME
-from tumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseTrainingPipeline, STAGE_NAME
+from tumorClassifier.pipeline.stage_01_dataingestion import DataIngestionTrainingPipeline
+from tumorClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseTrainingPipeline
 
+
+STAGE_NAME = "Data Ingestion"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseTrainingPipeline()
-    obj.main()
+    ingest_data = DataIngestionTrainingPipeline()
+    ingest_data.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base = PrepareBaseTrainingPipeline()
+    prepare_base.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
 except Exception as e:
     logger.exception(e)
